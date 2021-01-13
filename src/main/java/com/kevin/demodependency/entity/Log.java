66bc413210f0b1101be4518f9a406bc514a3b8cc
panghaiming <kevinatgz@qq.com>
2021-01-13 13:53:23 +0800

    log

--- src/main/java/com/kevin/demodependency/entity/Log.java
+++ src/main/java/com/kevin/demodependency/entity/Log.java
@@ -1,9 +1,13 @@
 package com.kevin.demodependency.entity;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
 public class Log implements Serializable {
 
-
+    private Date createTime;
 
 }
